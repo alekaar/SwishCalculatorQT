21264f1a3ebbbebaa8122cf5a84b450d23743266
alekaar <sternkaar@gmail.com>
2017-08-26 21:35:00 +0200

    working on calculation method

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -44,43 +44,30 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-/*void MainWindow::on_pushButton_clicked()
-{
-    QLabel *t1 = new QLabel(ui->lineEdit->text());
-    QString s = ui->lineEdit_1->text();
-
-    bool ok;
-    s.toInt(&ok, 10);
-
-    if(ok){
-        QLabel *t2 = new QLabel(s);
-
-        QBoxLayout *l = new QBoxLayout(QBoxLayout::LeftToRight);
-
-        l->addWidget(t1);
-        l->addWidget(t2);
-        ui->verticalLayout_2->addLayout(l);
-    }
-}
-*/
 void MainWindow::on_addPerson_clicked()
 {
+    //create neew layout for lines
     QBoxLayout *v = new QBoxLayout(QBoxLayout::LeftToRight);
 
+    //create line for name
     QLineEdit *name = new QLineEdit();
     name->setFixedWidth(100);
     name->setObjectName("name");
 
+    //create line for money
     QLineEdit *money = new QLineEdit();
     money->setFixedWidth(40);
     money->setObjectName("money");
 
 
+    //add them to layout
     v->addWidget(name);
     v->addWidget(money);
 
+    //save them to list
     pers->prepend(qMakePair(name, money));
 
+    //add layout to values layout
     values->addLayout(v);
     values->setAlignment(v, Qt::AlignLeft);
 }
@@ -89,15 +76,64 @@ void MainWindow::on_computeMon_clicked()
 {
 
     //calculate total amount
-    int total = 0;
+    float total = 0;
 
     for(int i = 0; i < pers->size(); i++){
         total += pers->at(i).second->text().toInt();
     }
     ui->total->setText("Total: " + QString::number(total));
+
     //PER PERSON
-    int perPerson = total/pers->size();
+    QList<QPair <QLineEdit *, float> > *owing = new QList<QPair<QLineEdit *, float>>();
+    QList<QPair <QLineEdit *, float> > *payedMore = new QList<QPair<QLineEdit *, float>>();
+
+    float perPerson = total/pers->size();
     qDebug() << perPerson;
+    //categorize
+    for(int i = 0; i < pers->size(); i++){
+        //not payed enough, add to owing
+        if(pers->at(i).second->text().toFloat() < perPerson){
+            owing->append(qMakePair(pers->at(i).first, pers->at(i).second->text().toFloat()));
+        }
+        //payed too much, add to payedMore
+        else if(pers->at(i).second->text().toFloat() > perPerson){
+            payedMore->append(qMakePair(pers->at(i).first, pers->at(i).second->text().toFloat()));
+        }
+    }
+
+    QString output;
+    //start planning swishing
+    for(int i = 0; i < owing->size(); i++){
+        float leftToPay = perPerson - owing->at(i).second;
+        qDebug() << leftToPay;
+        //check who to swish
+        //TODO remove from payedMore when
+        for(int j = 0; j < payedMore->size() && leftToPay > 0; j++){
+            if(payedMore->at(j).second - leftToPay >= perPerson){
+
+                float newBalance = payedMore->at(j).second - leftToPay;
+                payedMore->replace(j, qMakePair(payedMore->at(j).first, newBalance));
+
+                output.append("@" + owing->at(i).first->text() + " -> " + QString::number(leftToPay)
+                              + "@" + payedMore->at(j).first->text() + " \n");
+                leftToPay = perPerson;
+            }
+            else{
+                float tooMuch = (perPerson - payedMore->at(j).second);
+                leftToPay += tooMuch;
+
+                float newBalance = payedMore->at(j).second - tooMuch;
+                payedMore->replace(j, qMakePair(payedMore->at(j).first, newBalance));
+
+                output.append("@" + owing->at(i).first->text() + " -> " + QString::number(tooMuch)
+                              + " @" + payedMore->at(j).first->text() + " \n");
+            }
+        }
+
+    }
+    qDebug() << output;
+
+
 
 
 }
